cbe721c3f3ea5ae93380d4756b04076d715fed42
GusVilla303 <gusvilla303@gmail.com>
2017-11-08 11:35:22 -0700

    Add '/landmarks/new' action, route, and form in view. Related specs are passing

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,19 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks/new' do
+    erb :"landmarks/new"
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+
+    erb :'landmarks/show'
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(params[:landmark])
+
+    redirect :"/landmarks/#{@landmark.id}"
+  end
+
 end
