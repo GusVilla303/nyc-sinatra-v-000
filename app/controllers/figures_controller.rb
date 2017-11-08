a0431b0d21d806c4c071afcbdd0ba3021a0befe0
GusVilla303 <gusvilla303@gmail.com>
2017-11-07 19:42:18 -0700

    Add functionality to create a new figure with title, and create a new figure with a landmark.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,20 @@
 class FiguresController < ApplicationController
 
+  get '/figures/new' do
+    @landmarks = Landmark.all
+    @titles = Title.all
+    erb :'figures/new'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+
+    erb :"figures/#{@figure.id}"
+  end
+
 end
