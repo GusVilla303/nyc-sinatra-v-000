269714079357d6103a5e9b2a2b58d0d6dfdf113a
GusVilla303 <gusvilla303@gmail.com>
2017-11-08 11:56:52 -0700

    Add '/landmarks/:id/edit' action, route, and view. All specs are passing.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,5 +1,11 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks' do
+    @landmarks = Landmark.all
+
+    erb :'landmarks/index'
+  end
+
   get '/landmarks/new' do
     erb :"landmarks/new"
   end
@@ -13,7 +19,21 @@ class LandmarksController < ApplicationController
   post '/landmarks' do
     @landmark = Landmark.create(params[:landmark])
 
-    redirect :"/landmarks/#{@landmark.id}"
+    redirect :"landmarks/#{@landmark.id}"
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+
+    erb :"landmarks/edit"
+  end
+
+  post '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.update(params[:landmark])
+    @landmark.save
+
+    redirect :"landmarks/#{@landmark.id}"    
   end
 
 end
